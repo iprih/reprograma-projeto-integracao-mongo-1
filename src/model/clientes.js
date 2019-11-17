4d4664fa7bcd75c0b67fc187fe535bc3e073ddb9
iprih <priscila.silva076@gmail.com>
2019-11-17 19:09:00 -0300

    initial commit

---
+++ src/model/clientes.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const ClientesSchema = new mongoose.Schema({
+
+
+nome: {type, String},
+email: {type, String},
+cpf: {type, Number},
+dataNascimento: {type, Date},
+estadoCivil: {type, String},
+telefone: {type, Number},
+comprou: {type, Boolean}},
+{versionKey: false})
+
+const Clientes = mongoose.model('Clientes', ClientesSchema)
+
+module.exports = Clientes
